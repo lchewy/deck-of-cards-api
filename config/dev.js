8556c3d7f79c0f267b5732869f3e9d3e2d0cbaeb
Leslie Chiu <chiu.les1@gmail.com>
2018-06-05 14:45:36 -0400

    test deploy

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    sequelizeURI: "fiftytwo",
+    sequelizeUserName: "leslie",
+    sequelizePassword: null,
+    sessionSecret: "bja;dlfkbagkbjhsgkjblksjgbhseiuh2bsbnjnfadjnks"
+}
---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  sequelizeURI: process.env.SEQUELIZE_URI,
+  sequelizeUserName: process.env.SEQUELIZE_USER_NAME,
+  sequelizePassword: process.env.SEQUELIZE_PASSWORD,
+  sessionSecret: process.env.SESSION_SECRET
+};
